0ab6362c1c9ee2a7c59963f671fe8e43eb4202ad
prudhvi2208 <74950898+prudhvi2208@users.noreply.github.com>
2022-08-12 00:26:47 +0530

    Create use.groovy

---
+++ var/use.groovy
@@ -0,0 +1,4 @@
+## We have to add shared libraries from github repo to shared libraries on jenkins where the pipeline is written
+
+@Library('shared-library-name') _
+jenkinsForJava 'https://github.com/iamvickyav/spring-boot-data-H2-embedded.git'
